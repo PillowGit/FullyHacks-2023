73170fc450e9da367d5c4f3890965ad88366390b
Gabrielius Gintalas <gabrieliusgintalas@csu.fullerton.edu>
2023-04-09 07:07:58 -0700

    Index almost done, need to make color shift
    Co-authored-by: Pillow <PillowGit@users.noreply.github.com>
    Co-authored-by: Ashley Chan <Ashleyc417@users.noreply.github.com>

--- navbarbutton.js
+++ navbarbutton.js
@@ -1,5 +1,21 @@
-document
-  .querySelector(".sidebar .togglebutton")
-  .addEventListener("click", function () {
-    document.querySelector(".sidebar").classList.toggle("active");
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".togglebutton");
+  const sidebar = document.querySelector(".sidebar");
+  const closeBtn = document.querySelector(".closebutton");
+
+  toggleButton.addEventListener("click", function () {
+    sidebar.classList.toggle("active");
   });
+
+  closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("active");
+  });
+
+  toggleButton.addEventListener("mouseover", function () {
+    toggleButton.classList.add("hover");
+  });
+
+  toggleButton.addEventListener("mouseout", function () {
+    toggleButton.classList.remove("hover");
+  });
+});
